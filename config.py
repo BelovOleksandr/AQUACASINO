ae0f28f3277b859a7c641a7454df7b82d900c35f
BelovOleksandr <belovvmaksym@gmail.com>
2025-04-05 14:00:41 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+TOKEN = "" # Токен бота
+CHANNEL_ID = -100дад # ID канала со ставками
+ADMINS = [б] # ID админов
+BET_URL = "http://t.me/send?start=IV2r4tr4ZuYo" # Ссылка на счет
+CRYPTOPAY_TOKEN = "ка" # Тут CryptoPay токен
+WIN_IMAGE = "win.jpg" # Название файла с картинкой выигрыша
+LOSE_IMAGE = "lose.jpg" # Название файла с картинкой проигрыша
+LOGS_ID = -100минус 100 не убирай # ID канала с оплатами
+
+RULES_LINK = "https://t.me/c/2173472320/1029" # Ссылка на канал с правилами
+NEWS_LINK = "https://t.me/+rwY_Wce8gAo5Yzdh" # Ссылка на новостной канал
+OWNER_LINK = "https://t.me/velfrid" # Ссылка на владельца/поддержку
+PEREHOD_LINK = "@AQUA_Bets" # Ссылка на переходник
+BETS_LINK = "https://t.me/+rwY_Wce8gAo5Yzdh" # Ссылка на канал ставок
+CASINO_NAME = "AQUA.Bet" # Название казино
